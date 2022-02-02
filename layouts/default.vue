26b1534da09e7e1597119d9a04052ce9e414c792
ProsMB <pros@mango-byte.com>
2022-02-02 11:37:28 +0700

    #2 Start coding on navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,41 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <v-navigation-drawer
+      class="grey lighten-1"
       v-model="drawer"
-      :mini-variant="miniVariant"
       :clipped="clipped"
+      temporary
       fixed
       app
     >
       <v-list>
+          <v-list-content>
+            <v-list-item-title class="text-center ">
+              <h2 class="accent--text font-weight-medium text-shadow">Elegant</h2>
+              <h4 class="display-1 font-weight-medium text-shadow">FURNITURE</h4>
+              <h2 class="accent--text font-weight-medium text-shadow  mb-2">Modern</h2>
+            </v-list-item-title>
+          </v-list-content>
+      </v-list>
+      <hr>
+      <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in sellerNav"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+        <hr>
+        <v-list-item
+          v-for="(item, i) in managerNav"
           :key="i"
           :to="item.to"
           router
@@ -25,69 +51,35 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
+      class="grey lighten-1"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+       <v-icon size="36px" @click.stop="drawer = !drawer" class="ml-lg-12 ml-sm-6" >mdi-menu</v-icon>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <input class="input-search" type="text" placeholder="Search for furniture...">
+      <v-btn icon class="mr-lg-6">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
+      <v-badge
+        class="mr-lg-12 mr-sm-6 mt-3"
+        bordered
+        color="error"
+        :icon="number"
+        overlap
+      >
+        <v-btn icon >
+          <v-icon class="mb-3" size="36px">mdi-cart-outline</v-icon>
+        </v-btn>
+      </v-badge>
     </v-app-bar>
-    <v-main>
+    <!-- <v-main>
       <v-container>
         <Nuxt />
       </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-main> -->
   </v-app>
 </template>
-
 <script>
 export default {
   name: 'DefaultLayout',
@@ -96,23 +88,55 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
+      sellerNav: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/home'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          icon: 'mdi-desk-lamp',
+          title: 'Furniture',
           to: '/inspire'
+        },
+         {
+          icon: 'mdi-brightness-percent',
+          title: 'Discount',
+          to: '/discount'
+        }
+      ],
+      managerNav:[
+        {
+          icon: 'mdi-account-cog',
+          title: 'Management',
+          to: '/management'
+        },
+         {
+          icon: 'mdi-finance',
+          title: 'Statistic',
+          to: '/statistic'
         }
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      number:"28"
     }
   }
 }
 </script>
+<style scoped>
+.input-search{
+  background: white;
+  outline: none;
+  height: 70%;
+  width: 30%;
+  padding: 5px;
+  border-radius:5px;
+}
+.input-search::placeholder{
+  text-align: center;
+}
+.text-shadow{
+  text-shadow: 0px 10px 6px #464545ef;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
         <VuetifyLogo />
+        
       </v-card>
       <v-card>
         <v-card-title class="headline">
