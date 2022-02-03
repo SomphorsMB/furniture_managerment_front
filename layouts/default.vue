253b392ccf7e48814ab813a0e6ea66ef836027bb
BunsalMB <bunsal@mango-byte.com>
2022-02-03 09:49:17 +0700

    #0 pull request of navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,142 +1,114 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
-      class="grey lighten-1"
-      v-model="drawer"
-      :clipped="clipped"
-      temporary
-      fixed
-      app
-    >
-      <v-list>
-          <v-list-content>
-            <v-list-item-title class="text-center ">
-              <h2 class="accent--text font-weight-medium text-shadow">Elegant</h2>
-              <h4 class="display-1 font-weight-medium text-shadow">FURNITURE</h4>
-              <h2 class="accent--text font-weight-medium text-shadow  mb-2">Modern</h2>
-            </v-list-item-title>
-          </v-list-content>
-      </v-list>
-      <hr>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in sellerNav"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+<v-app light>
+    <v-navigation-drawer class="grey lighten-1" v-model="drawer" :clipped="clipped" temporary fixed app>
+        <v-list>
+            <v-list-content>
+                <v-list-item-title class="text-center ">
+                    <h2 class="accent--text font-weight-medium text-shadow">Elegant</h2>
+                    <h4 class="display-1 font-weight-medium text-shadow">FURNITURE</h4>
+                    <h2 class="accent--text font-weight-medium text-shadow  mb-2">Modern</h2>
+                </v-list-item-title>
+            </v-list-content>
+        </v-list>
         <hr>
-        <v-list-item
-          v-for="(item, i) in managerNav"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+        <v-list>
+            <v-list-item v-for="(item, i) in sellerNav" :key="i" :to="item.to" router exact>
+                <v-list-item-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+            </v-list-item>
+            <hr>
+            <v-list-item v-for="(item, i) in managerNav" :key="i" :to="item.to" router exact>
+                <v-list-item-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      class="grey lighten-1"
-      fixed
-      app
-    >
-       <v-icon size="36px" @click.stop="drawer = !drawer" class="ml-lg-12 ml-sm-6" >mdi-menu</v-icon>
-      <v-spacer />
-      <input class="input-search" type="text" placeholder="Search for furniture...">
-      <v-btn icon class="mr-lg-6">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-badge
-        class="mr-lg-12 mr-sm-6 mt-3"
-        bordered
-        color="error"
-        :icon="number"
-        overlap
-      >
-        <v-btn icon >
-          <v-icon class="mb-3" size="36px">mdi-cart-outline</v-icon>
+    <v-app-bar class="grey lighten-1" fixed app>
+        <v-icon size="36px" @click.stop="drawer = !drawer" class="ml-lg-12 ml-sm-6">mdi-menu</v-icon>
+        <v-spacer />
+        <input class="input-search" type="text" placeholder="Search for furniture...">
+        <v-btn icon class="mr-lg-6">
+            <v-icon>mdi-magnify</v-icon>
         </v-btn>
-      </v-badge>
+        <v-badge class="mr-lg-12 mr-sm-6 mt-3" bordered color="error" :icon="number" overlap>
+            <v-btn icon>
+                <v-icon class="mb-3" size="36px">mdi-cart-outline</v-icon>
+            </v-btn>
+        </v-badge>
     </v-app-bar>
     <!-- <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main> -->
-  </v-app>
+</v-app>
 </template>
+
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      sellerNav: [
-        {
-          icon: 'mdi-home',
-          title: 'Home',
-          to: '/home'
-        },
-        {
-          icon: 'mdi-desk-lamp',
-          title: 'Furniture',
-          to: '/inspire'
-        },
-         {
-          icon: 'mdi-brightness-percent',
-          title: 'Discount',
-          to: '/discount'
+    name: 'DefaultLayout',
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            sellerNav: [{
+                    icon: 'mdi-home',
+                    title: 'Home',
+                    to: '/home'
+                },
+                {
+                    icon: 'mdi-desk-lamp',
+                    title: 'Furniture',
+                    to: '/inspire'
+                },
+                {
+                    icon: 'mdi-brightness-percent',
+                    title: 'Discount',
+                    to: '/discount'
+                }
+            ],
+            managerNav: [{
+                    icon: 'mdi-account-cog',
+                    title: 'Management',
+                    to: '/management'
+                },
+                {
+                    icon: 'mdi-finance',
+                    title: 'Statistic',
+                    to: '/statistic'
+                }
+            ],
+            miniVariant: false,
+            number: "28"
         }
-      ],
-      managerNav:[
-        {
-          icon: 'mdi-account-cog',
-          title: 'Management',
-          to: '/management'
-        },
-         {
-          icon: 'mdi-finance',
-          title: 'Statistic',
-          to: '/statistic'
-        }
-      ],
-      miniVariant: false,
-      number:"28"
     }
-  }
 }
 </script>
+
 <style scoped>
-.input-search{
-  background: white;
-  outline: none;
-  height: 70%;
-  width: 30%;
-  padding: 5px;
-  border-radius:5px;
-}
-.input-search::placeholder{
-  text-align: center;
-}
-.text-shadow{
-  text-shadow: 0px 10px 6px #464545ef;
+.input-search {
+    background: white;
+    outline: none;
+    height: 70%;
+    width: 30%;
+    padding: 5px;
+    border-radius: 5px;
 }
 
+.input-search::placeholder {
+    text-align: center;
+}
+
+.text-shadow {
+    text-shadow: 0px 10px 6px #464545ef;
+}
 </style>
