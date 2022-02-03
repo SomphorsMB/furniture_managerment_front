97e03da7528ae8b7bbe62db307da5c3edde669e9
BunsalMB <bunsal@mango-byte.com>
2022-02-03 14:10:15 +0700

    #0 Semi-final commit of UI

--- components/product_management.vue
+++ components/product_management.vue
@@ -8,17 +8,13 @@
             <template v-slot:activator="{ on, attrs }">
                 <div class="float-right">
                     <v-btn class="mx-2" fab color="C4C4C4" v-bind="attrs" v-on="on">
-                        <v-icon dark>
-                            mdi-plus
-                        </v-icon>
+                        <v-icon dark> mdi-plus </v-icon>
                     </v-btn>
                 </div>
             </template>
             <v-card>
                 <div align="center" class="grey lighten-3 pa-3">
-                    <h2>
-                        Create a Product
-                    </h2>
+                    <h2>Create a Product</h2>
                 </div>
                 <v-card-text>
                     <v-form>
@@ -50,9 +46,9 @@
                                 </v-col>
                                 <v-col cols="12" sm="2" class="file">
                                     <label for="file-input">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV9ef4mu_ntPiqBjBlsGQNRNDLBKNILEnBoBP1rJlD_0P3cQ_f3DbGdeR-i5PAffS7oo8&usqp=CAU" width="40px" height="40px"/>
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV9ef4mu_ntPiqBjBlsGQNRNDLBKNILEnBoBP1rJlD_0P3cQ_f3DbGdeR-i5PAffS7oo8&usqp=CAU" width="40px" height="40px" />
                                     </label>
-                                    <input type="file" show-size counter multiple label="File input" id="file-input" class="file-input"/>
+                                    <input type="file" show-size counter multiple label="File input" id="file-input" class="file-input" />
                                 </v-col>
                             </v-row>
                         </v-container>
@@ -73,18 +69,27 @@
             <template v-slot:body="{ items }">
                 <tbody>
                     <tr v-for="product in items" :key="product.id">
-                        <td><img width="40px" height="40px" src="https://media.istockphoto.com/photos/modern-designer-chair-on-white-background-texture-chair-picture-id899418544?b=1&k=20&m=899418544&s=170667a&w=0&h=KKBpKvOtEXd2yVmRbQ6zYOnJmQ00Q6E-ks18F8_yrFQ="></td>
+                        <td>
+                            <img width="40px" height="40px" src="https://media.istockphoto.com/photos/modern-designer-chair-on-white-background-texture-chair-picture-id899418544?b=1&k=20&m=899418544&s=170667a&w=0&h=KKBpKvOtEXd2yVmRbQ6zYOnJmQ00Q6E-ks18F8_yrFQ=" />
+                        </td>
                         <td>{{ product.name }}</td>
                         <td>{{ product.brand }}</td>
                         <td>{{ product.country }}</td>
                         <td>{{ product.unit }}</td>
                         <td>{{ product.price }}</td>
+                        <td>{{ product.discount }}</td>
                         <td>
                             <v-icon small color="red" class="delete mr-1" @click="deleteItem">
                                 mdi-delete
                             </v-icon>
                             <v-icon small color="#00E676" class="edit mr-1">
                                 mdi-pencil
+                            </v-icon>
+                            <v-icon small color="#00E676" class="edit" @click="addAmount">
+                                mdi-plus
+                            </v-icon>
+                            <v-icon small color="#00E676" class="edit" @click="discountItem">
+                                mdi-percent
                             </v-icon>
                         </td>
                     </tr>
@@ -93,9 +98,7 @@
                 <v-dialog v-model="dialogDelete" max-width="350px">
                     <v-card>
                         <div align="center" class="grey lighten-3 pa-3">
-                            <h2>
-                                Delete this product
-                            </h2>
+                            <h2>Delete this product</h2>
                         </div>
                         <div align="center" class="mt-3 mb-3">
                             <p>Are you sure you want to delete this item?</p>
@@ -108,6 +111,57 @@
                     </v-card>
                 </v-dialog>
                 <!--  -->
+                <v-dialog v-model="dialogAdd" max-width="350px">
+                    <v-card>
+                        <div align="center" class="grey lighten-3 pa-3">
+                            <h2>Increase Product</h2>
+                        </div>
+                        <div align="center">
+                            <v-col cols="12" class="mt-6">
+                                <v-text-field v-model="amount" label="Amount" dense small outlined clearable></v-text-field>
+                            </v-col>
+                        </div>
+                        <v-card-actions>
+                            <v-btn color="red darken-1" text @click="closeDelete">Cancel</v-btn>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="addAmountConfirm">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+                <!--  -->
+                <v-dialog v-model="dialogDiscount" max-width="350px">
+                    <v-card>
+                        <div align="center" class="grey lighten-3 pa-3">
+                            <h2>Discount Product</h2>
+                        </div>
+                        <div align="center">
+                            <v-col cols="12" class="mt-6">
+                                <v-text-field v-model="discount" label="Discount %" dense small outlined clearable></v-text-field>
+                            </v-col>
+                            <v-col cols="12" class="start_date">
+                                <v-menu v-model="start_date" :close-on-content-click="false" max-width="290">
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-text-field :value="computedDateFormattedMomentjs" dense small outlined clearable label="Start Date" v-bind="attrs" v-on="on"></v-text-field>
+                                    </template>
+                                    <v-date-picker v-model="date" @change="start_date = false"></v-date-picker>
+                                </v-menu>
+                            </v-col>
+                            <v-col cols="12" class="end_date">
+                                <v-menu v-model="end_date" :close-on-content-click="false" max-width="290">
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-text-field :value="computedDateFormattedMomentjs" dense small outlined clearable label="End Date" v-bind="attrs" v-on="on"></v-text-field>
+                                    </template>
+                                    <v-date-picker v-model="date" @change="end_date = false"></v-date-picker>
+                                </v-menu>
+                            </v-col>
+                        </div>
+                        <v-card-actions>
+                            <v-btn color="red darken-1" text @click="closeDelete">Cancel</v-btn>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="discountItemConfirm">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </template>
         </v-data-table>
     </div>
@@ -115,117 +169,169 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import moment from 'moment'
+import { format, parseISO } from 'date-fns'
 export default {
     data() {
         return {
             dialog: false,
             dialogDelete: false,
-            name: '',
-            categories: ['Table', 'Chair', 'Sofa', 'Bed', 'Dinning table', 'Lamp'],
-            brands: ['PAINCA', 'DESIREE', 'BONALDO', 'CATTELAN', 'PEDRO'],
-            sizes: ['Small', 'Medium', 'Large'],
-            colors: ['Red', 'Blue', 'Grey', 'Pink', 'Yellow', 'Cyan', 'Margenta', 'Brown', 'Silver', 'Gold', 'Green', 'White', 'Black', 'Orange', 'Purple'],
+            dialogAdd: false,
+            dialogDiscount: false,
+            name: "",
+            discount: "0%",
+            amount: 0,
+            date: format(parseISO(new Date().toISOString()), 'yyyy-MM-dd'),
+            start_date: false,
+            end_date: false,
+            categories: ["Table", "Chair", "Sofa", "Bed", "Dinning table", "Lamp"],
+            brands: ["PAINCA", "DESIREE", "BONALDO", "CATTELAN", "PEDRO"],
+            sizes: ["Small", "Medium", "Large"],
+            colors: [
+                "Red",
+                "Blue",
+                "Grey",
+                "Pink",
+                "Yellow",
+                "Cyan",
+                "Margenta",
+                "Brown",
+                "Silver",
+                "Gold",
+                "Green",
+                "White",
+                "Black",
+                "Orange",
+                "Purple",
+            ],
             unit: 0,
-            price: '',
-            headers: [
-                {
-                    text: 'Furniture',
-                    value: 'furniture',
-                    sortable: false
+            price: "",
+            headers: [{
+                    text: "Furniture",
+                    value: "furniture",
+                    sortable: false,
                 },
                 {
-                    text: 'Name',
-                    value: 'name'
+                    text: "Name",
+                    value: "name",
                 },
                 {
-                    text: 'Brand',
-                    value: 'brand'
+                    text: "Brand",
+                    value: "brand",
                 },
                 {
-                    text: 'Country',
-                    value: 'country'
+                    text: "Country",
+                    value: "country",
                 },
                 {
-                    text: 'Unit',
-                    value: 'unit'
+                    text: "Unit",
+                    value: "unit",
                 },
                 {
-                    text: 'Price',
-                    value: 'price',
+                    text: "Price",
+                    value: "price",
                 },
                 {
-                    text: 'Discount',
-                    value: 'discount',
+                    text: "Discount",
+                    value: "discount",
                 },
                 {
-                    text: 'Action',
-                    value: 'action',
-                    sortable: false
+                    text: "Action",
+                    value: "action",
+                    sortable: false,
                 },
             ],
-            products: [
-                {
-                    name: 'Chair',
-                    brand: 'Benaldo',
-                    country: 'Italy',
+            products: [{
+                    name: "Chair",
+                    brand: "Benaldo",
+                    country: "Italy",
                     unit: 2,
-                    price: '150$'
+                    price: "150$",
+                    discount: "20%",
                 },
                 {
-                    name: 'Chair',
-                    brand: 'Benaldo',
-                    country: 'Italy',
+                    name: "Chair",
+                    brand: "Benaldo",
+                    country: "Italy",
                     unit: 2,
-                    price: '150$'
+                    price: "150$",
+                    discount: "40%",
                 },
                 {
-                    name: 'Chair',
-                    brand: 'Benaldo',
-                    country: 'Italy',
+                    name: "Chair",
+                    brand: "Benaldo",
+                    country: "Italy",
                     unit: 2,
-                    price: '150$'
+                    price: "150$",
+                    discount: "15%",
                 },
                 {
-                    name: 'Chair',
-                    brand: 'Benaldo',
-                    country: 'Italy',
+                    name: "Chair",
+                    brand: "Benaldo",
+                    country: "Italy",
                     unit: 2,
-                    price: '150$'
+                    price: "150$",
+                    discount: "No discount",
                 },
                 {
-                    name: 'Chair',
-                    brand: 'Benaldo',
-                    country: 'Italy',
+                    name: "Chair",
+                    brand: "Benaldo",
+                    country: "Italy",
                     unit: 2,
-                    price: '150$'
-                }
-            ]
-        }
+                    price: "150$",
+                    discount: "99%",
+                },
+            ],
+        };
+    },
+    computed: {
+        computedDateFormattedMomentjs () {
+            return this.date ? moment(this.date).format('dddd, MMMM Do YYYY') : ''
+        },
     },
     methods: {
         closeDelete() {
             this.dialogDelete = false;
+            this.dialogAdd = false;
+            this.dialogDiscount = false;
         },
+
         deleteItemConfirm() {
             this.closeDelete();
         },
+
         deleteItem() {
             this.dialogDelete = true;
         },
-    },
-    mounted() {
 
-    }
-}
+        addAmountConfirm() {
+            this.closeDelete();
+        },
+
+        addAmount() {
+            this.dialogAdd = true;
+        },
+
+        discountItemConfirm() {
+            this.closeDelete();
+        },
+
+        discountItem() {
+            this.dialogDiscount = true;
+        },
+    },
+    mounted() {},
+};
 </script>
 
 <style scoped>
 .seller-management {
-    text-shadow: 0px 2px 4px #3B3B3B;
+    text-shadow: 0px 2px 4px #3b3b3b;
 }
 
-.delete:hover, .edit:hover {
+.delete:hover,
+.edit:hover {
     cursor: pointer;
 }
 
@@ -233,11 +339,18 @@ export default {
     margin-top: 100px;
 }
 
-.name, .category {
+.name,
+.category {
     margin-top: 15px;
 }
 
-.brand, .size, .unit, .color, .price, .file, .text-area {
+.brand,
+.size,
+.unit,
+.color,
+.price,
+.file,
+.text-area {
     margin-top: -30px;
 }
 
@@ -247,5 +360,9 @@ export default {
 
 .save {
     margin-top: -40px;
+}
+
+.start_date, .end_date {
+    margin-top: -30px;
 }
 </style>
